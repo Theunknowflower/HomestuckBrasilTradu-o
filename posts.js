18efa1c10512ea9643901631852fe3fc5df48e5a
Theunknowflower <rafaellamalfatimachado18@gmail.com>
2025-09-19 00:17:59 -0300

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,3 +1,13 @@
+// importa cliente do Supabase
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+// inicializa
+const SUPABASE_URL = "https://vhopcdzemdiqtvrwmqqo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZob3BjZHplbWRpcXR2cndtcXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMjc2MTUsImV4cCI6MjA3MzgwMzYxNX0.j8podlPF9lBz2LfzDq1Z0NYF2QA3tQRK-tOIalWz2sI"; // copia da dashboard
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
 import { supabase, getUser } from "./supabase.js";
 import { unlockAchievement } from "./achievements.js";
 
