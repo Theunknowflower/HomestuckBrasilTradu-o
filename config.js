4d2ec7e5a7efafb824f59d586886ca8ea0b3cf90
Theunknowflower <rafaellamalfatimachado18@gmail.com>
2025-09-19 12:15:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,6 @@ const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 
 // expõe globalmente
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_URL = "https://vhopcdzemdiqtvrwmqqo.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZob3BjZHplbWRpcXR2cndtcXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMjc2MTUsImV4cCI6MjA3MzgwMzYxNX0.j8podlPF9lBz2LfzDq1Z0NYF2QA3tQRK-tOIalWz2sI";
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
