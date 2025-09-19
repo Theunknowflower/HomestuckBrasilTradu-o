ef96ad9ebf46b2e712b62ac43652ca865d3f6b91
Theunknowflower <rafaellamalfatimachado18@gmail.com>
2025-09-19 00:00:04 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,13 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://SEU-PROJETO.supabase.co",
-  "PUBLIC-ANON-KEY"
+  "https://vhopcdzemdiqtvrwmqqo.supabase.co", // troque pelo seu
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZob3BjZHplbWRpcXR2cndtcXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMjc2MTUsImV4cCI6MjA3MzgwMzYxNX0.j8podlPF9lBz2LfzDq1Z0NYF2QA3tQRK-tOIalWz2sI"                  // troque pelo seu
 );
 
-// helper para pegar usuário logado
 export async function getUser() {
   const { data: { user } } = await supabase.auth.getUser();
   return user;
 }
-
