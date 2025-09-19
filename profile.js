b01fa63814a02eecb572cae52304b4c29f33faac
Theunknowflower <rafaellamalfatimachado18@gmail.com>
2025-09-18 23:57:43 -0300

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,27 @@
+import { supabase, getUser } from "./supabase.js";
+
+export async function loadProfile() {
+  const user = await getUser();
+  if (!user) return;
+
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("username, avatar_url")
+    .eq("id", user.id)
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
+export async function updateProfile(updates) {
+  const user = await getUser();
+  if (!user) return;
+
+  const { error } = await supabase
+    .from("profiles")
+    .update(updates)
+    .eq("id", user.id);
+
+  if (error) throw error;
+}
