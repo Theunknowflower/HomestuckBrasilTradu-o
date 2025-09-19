bc5bdea53087d6a8d9fd10f51196463a1b18afcb
Theunknowflower <rafaellamalfatimachado18@gmail.com>
2025-09-19 15:24:29 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Detecta login via OTP ou qualquer método
-supabase.auth.onAuthStateChange(async (event, session) => {
-  if (event === "SIGNED_IN") {
-    console.log("Usuário logado via OTP ou senha:", session.user);
-    loadUserProfile(session.user); // chama sua função de perfil
-  }
-});
